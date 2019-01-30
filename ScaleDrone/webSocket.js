63302416d23d3556846aefdd1b21660a3d3ca7bd
Isitea <isitea@isitea.net>
2019-01-30 14:59:53 +0900

    ScaleDrone 000

--- ScaleDrone/webSocket.js
+++ ScaleDrone/webSocket.js
@@ -1,2 +1,28 @@
 "use strict";
-const $channel = new Scaledrone( 'JRaMq4XI65Wxk6uq' );
+class SocketIO {
+    constructor ( channelId ) {
+        Object.defineProperties( this, {
+            port : {
+                value: new Promise( ( resolve, reject ) => {
+                    const $drone = new Scaledrone( channelId );
+                    $drone.on( 'open', () => resolve( $drone ) );
+                    $drone.on( 'error', reject );
+                } ),
+                configurable: true
+            }
+        } );
+    }
+
+    initialize () {
+        let port = Promise( ( resolve, reject ) => {
+            const $drone = new Scaledrone( 'JRaMq4XI65Wxk6uq' );
+            $drone.on( 'open', () => resolve( $drone ) );
+            $drone.on( 'error', reject );
+        } );
+        port.then( port => Object.defineProperties( this, { port: { value: port } } ) );
+
+        return port;
+    }
+}
+
+new SocketIO( 'JRaMq4XI65Wxk6uq' )
